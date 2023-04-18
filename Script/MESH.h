77df7aeca87f6159b5b7fe49acd1a503b2ecda6e
GakuIshimoto <ishimoty0124@gmail.com>
2023-04-19 02:19:16 +0900

    ※ビルドは通らない
    【目標】
    Renderの処理を一手に担っているMeshクラスをShaderクラス、Materialクラスに機能分離する
    
    【やったこと】
    cShaderクラスを作成し、Shaderの初期化とUpdate処理をこのクラスに移動
    
    【Todo】
    ConstantBuffer→Shaderクラスに持たせる
    IndexBuffer　 →Materialクラスに持たせる
    
    Shaderインスタンス→Listにする
    keyがShaderインスタンス、ValueがMaterialインスタンスの配列のUnoderdMapを作成する

--- Script/MESH.h
+++ Script/MESH.h
@@ -6,6 +6,9 @@
 #include <d3dx10.h>
 #include <d3dx11.h>
 #include <d3dCompiler.h>
+#include <memory>
+#include <unordered_map>
+#include <string>
 
 #pragma comment(lib,"winmm.lib")
 #pragma comment(lib,"d3dx10.lib")
@@ -26,15 +29,6 @@ struct MY_VERTEX
 	D3DXVECTOR2 vUV;
 };
 
-//Simpleシェーダー用のコンスタントバッファーのアプリ側構造体 もちろんシェーダー内のコンスタントバッファーと一致している必要あり
-struct SIMPLECONSTANT_BUFFER0
-{
-	D3DXMATRIX mW;//ワールド行列
-	D3DXMATRIX mWVP;//ワールドから射影までの変換行列
-	D3DXVECTOR4 vLightDir;//ライト方向
-	D3DXVECTOR4 vEye;//カメラ位置
-};
-
 struct SIMPLECONSTANT_BUFFER1
 {
 	D3DXVECTOR4 vAmbient;//アンビエント光
@@ -52,9 +46,15 @@ struct MY_MATERIAL
 	CHAR szTextureName[110];//テクスチャーファイル名
 	ID3D11ShaderResourceView* pTexture;
 	DWORD dwNumFace;//そのマテリアルであるポリゴン数
+	LPWSTR shaderPath;
+	/// <summary>
+	/// インデックスバッファ
+	/// </summary>
+	ID3D11Buffer** m_ppIndexBuffer;
+
 	MY_MATERIAL()
 	{
-		ZeroMemory(this,sizeof(MY_MATERIAL));
+		ZeroMemory(this, sizeof(MY_MATERIAL));
 	}
 	~MY_MATERIAL()
 	{
@@ -70,36 +70,130 @@ class MESH
 public:
 	MESH();
 	~MESH();
-	HRESULT Init(ID3D11Device* pDevice,ID3D11DeviceContext* pContext);
+	HRESULT Init(ID3D11Device* pDevice, ID3D11DeviceContext* pContext);
 	HRESULT InitShader();
-	HRESULT LoadMaterialFromFile(LPSTR FileName,MY_MATERIAL** ppMaterial);
-	void Render(D3DXMATRIX& mView,D3DXMATRIX& mProj,D3DXVECTOR3& vLight,D3DXVECTOR3& vEye);
+	HRESULT LoadMaterialFromFile(LPSTR FileName, MY_MATERIAL** ppMaterial);
+	void Render(D3DXMATRIX& mView, D3DXMATRIX& mProj, D3DXVECTOR3& vLight, D3DXVECTOR3& vEye);
 
+	/// <summary>
+	/// 頂点数
+	/// </summary>
 	DWORD m_dwNumVert;
+	/// <summary>
+	/// ポリゴン数
+	/// </summary>
 	DWORD m_dwNumFace;
+	/// <summary>
+	/// 頂点バッファ
+	/// </summary>
 	ID3D11Buffer* m_pVertexBuffer;
-	ID3D11Buffer** m_ppIndexBuffer;
 
-	ID3D11InputLayout* m_pVertexLayout;
-	ID3D11VertexShader* m_pVertexShader;
-	ID3D11PixelShader* m_pPixelShader;
-	ID3D11Buffer* m_pConstantBuffer0;
-	ID3D11Buffer* m_pConstantBuffer1;
-
+	/// <summary>
+	/// マテリアル数（マテリアル数だけ描画する）
+	/// </summary>
 	DWORD m_dwNumMaterial;
+	/// <summary>
+	/// マテリアルバッファ配列
+	/// </summary>
 	MY_MATERIAL* m_pMaterial;
+	/// <summary>
+	/// Shader数
+	/// </summary>
+	DWORD m_dwNumShader;
+	/// <summary>
+	/// Shaderのリスト
+	/// </summary>
+	std::unordered_map<LPWSTR, cShader*> m_Shader;
+	/// <summary>
+	/// サンプラーステート
+	/// </summary>
 	ID3D11SamplerState* m_pSampleLinear;
-	ID3D11ShaderResourceView* m_pTexture;
 
-	D3DXVECTOR3 m_vPos;
-	float m_fYaw,m_fPitch,m_fRoll;
-	float m_fScale;
 
 private:
-	LPWSTR MODEL_PATH; //モデルのリソースパス
-	LPWSTR SHADER_PATH; //Shaderのリソースパス
+	// トランスフォーム情報
+	D3DXVECTOR3 m_vPos;
+	float m_fYaw, m_fPitch, m_fRoll;
+	float m_fScale;
 
+	/// <summary>
+	/// モデルのリソースパス
+	/// </summary>
+	LPWSTR MODEL_PATH;
+	/// <summary>
+	/// デバイス
+	/// </summary>
 	ID3D11Device* m_pDevice;
-	ID3D11DeviceContext *m_pDeviceContext;
-	HRESULT LoadStaticMesh(LPWSTR FileName);
+	/// <summary>
+	/// デバイスコンテキスト
+	/// </summary>
+	ID3D11DeviceContext* m_pDeviceContext;
+
+	/// <summary>
+	/// ファイルからMeshを読み込む
+	/// </summary>
+	/// <param name="FileName"></param>
+	/// <returns></returns>
+	HRESULT LoadResources(LPWSTR FileName);
+};
+
+class cShader {
+
+public:
+	//Simpleシェーダー用のコンスタントバッファーのアプリ側構造体 もちろんシェーダー内のコンスタントバッファーと一致している必要あり
+	struct SIMPLECONSTANT_BUFFER0
+	{
+		D3DXMATRIX mW;//ワールド行列
+		D3DXMATRIX mWVP;//ワールドから射影までの変換行列
+		D3DXVECTOR4 vLightDir;//ライト方向
+		D3DXVECTOR4 vEye;//カメラ位置
+	};
+
+
+
+	cShader(LPWSTR shaderPath, ID3D11Device* device, ID3D11DeviceContext* deviceContext);
+	~cShader()
+	{
+		SAFE_RELEASE(m_pVertexShader);
+		SAFE_RELEASE(m_pPixelShader);
+		SAFE_RELEASE(m_pConstantBuffer0);
+		SAFE_RELEASE(m_pConstantBuffer1);
+		SAFE_RELEASE(m_pVertexLayout);
+	}
+	HRESULT initShader();
+	HRESULT render(D3DXMATRIX& world, D3DXMATRIX& view, D3DXMATRIX& proj, D3DXMATRIX& mProj, D3DXVECTOR3& vLight, D3DXVECTOR3& vEye);
+
+	/// <summary>
+	/// Shaderパス
+	/// </summary>
+	LPWSTR m_ShaderPath;
+	/// <summary>
+	/// 頂点シェーダー
+	/// </summary>
+	ID3D11VertexShader* m_pVertexShader;
+	/// <summary>
+	/// ピクセルシェーダー
+	/// </summary>
+	ID3D11PixelShader* m_pPixelShader;
+	/// <summary>
+	/// コンスタントバッファ0
+	/// </summary>
+	ID3D11Buffer* m_pConstantBuffer0;
+	/// <summary>
+	/// コンスタントバッファ1
+	/// </summary>
+	ID3D11Buffer* m_pConstantBuffer1;
+	/// <summary>
+	/// 頂点レイアウト
+	/// </summary>
+	ID3D11InputLayout* m_pVertexLayout;
+	// 以下はRenderクラスをシングルトンにして、アクセスできるようにする
+	/// <summary>
+	/// デバイス
+	/// </summary>
+	ID3D11Device* m_pDevice;
+	/// <summary>
+	/// デバイスコンテキスト
+	/// </summary>
+	ID3D11DeviceContext* m_pDeviceContext;
 };
