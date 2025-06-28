db32135851c05fa2c945803cf3d80089559269bd
GakuIshimoto <ishimoty0124@gmail.com>
2025-06-29 08:21:11 +0900

    BRDF(途中)

--- Shader/PS_GBufferToBackBuffer.hlsl
+++ Shader/PS_GBufferToBackBuffer.hlsl
@@ -1,6 +1,8 @@
 #include "Resources.inc.hlsl"
 
-//Lambert
+//------------------------
+// Lambert
+//------------------------
 float4 lambert(float3 N, float3 L)
 {
     //Lambert反射
@@ -13,21 +15,19 @@ float4 lambert(float3 N, float3 L)
     return ambient * NL;
 }
 
-float4 phong(float3 N, float3 L, float3 V, float3 R)
+//------------------------
+// Cook-Torrance BRDF
+//------------------------
+float chiGGX(float v)
 {
-     //拡散反射（Lambert）
-    float3 diffuse = lambert(N, L);
-    
-    //鏡面反射
-    float spec = pow(saturate(dot(R, V)), g_Specular.r);
-    float3 speculer = float3(1, 1, 1) * spec;
-    
-    //環境光
-    float3 ambient = g_Ambient.rgb;
-    
-    return float4(ambient * diffuse + speculer, 1);
-
+    return v > 0 ? 1 : 0;
 }
+
+float4 GGX_Distribution()
+{
+}
+
+
 
 //テクスチャーを参照してレンダー　ピクセルシェーダー
 float4 main(VS_OUTPUT input) : SV_Target
@@ -39,18 +39,5 @@ float4 main(VS_OUTPUT input) : SV_Target
     float3 V = normalize(vWPos - g_vEye.xyz);
     float3 R = reflect(-L, N);
     
-    return phong(N, L, V, R);
-	
-	////取り出した情報をもとに計算
-	////環境光　項
- //   float4 ambient = g_Ambient;
-	////拡散反射光　項
- //   float NL = saturate(dot(N, L));
- //   float4 diffuse = (g_Diffuse / 2 + ambient / 2) * NL;
-	////鏡面反射光　項
- //   float3 reflect = normalize(2 * NL * N - L);
- //   float4 specular = pow(saturate(dot(reflect, eyeDir)), 4) * g_Specular;
-	
- //   float4 color = diffuse + specular;
- //   return color;
+    return lambert(N, L);
 }
